a834e1b3ae7538f4440d9dcd416662261529c91b
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-03-11 02:55:41 +0530

    fixed some issue

--- SonalKunj_wifi.c
+++ SonalKunj_wifi.c
@@ -6,18 +6,20 @@
 void wifiInit(void)
 {
 	uart_2_Init();
-	uart_2_Transmit(100);
+	uart_2_Transmit(0x01);
 }
 
 void wifiUpdateDatabase(unsigned char temp, unsigned char humid, float co2)
 {
-	uart_2_Transmit(101);
-	uart_2_Transmit(temp);
-	uart_2_Transmit(humid);
+	uart_2_Transmit(0x02);
+	uart_2_Transmit(temp/10 + '0');
+	uart_2_Transmit(temp%10 + '0');
+	uart_2_Transmit(humid/10 + '0');
+	uart_2_Transmit(humid%10 + '0');
 	uart_2_Transmit(co2/10 + '0');
 	uart_2_Transmit('.');
 	uart_2_Transmit((unsigned char)co2%10 + '0');
-	uart_2_Transmit(102);
+	uart_2_Transmit(0x03);
 }
 
 unsigned char wifiGetCommand(void)
